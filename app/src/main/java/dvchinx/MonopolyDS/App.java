7ecba5b73c1907b73014a810a19af13ee38ccea2
Jesús Flórez <71992811+dvchinx@users.noreply.github.com>
2024-12-01 18:55:43 -0500

    Initial Commit

---
+++ app/src/main/java/dvchinx/MonopolyDS/App.java
@@ -0,0 +1,11 @@
+package dvchinx.MonopolyDS;
+
+public class App {
+
+    public static void main(String[] args) {
+        View view = new View();
+        view.setVisible(true);
+        view.setResizable(false);
+        view.setLocationRelativeTo(null);
+    }
+}
